304df2da09748dc36e4e77a6c57e4907789f91cc
katelyn-jms302 <katelyn-jms302>
2014-12-08 09:19:17 -0500

    final profile complete

--- main.rb
+++ main.rb
@@ -22,25 +22,25 @@ get '/works' do
     erb :work
 end
 
-get '/tweets' do 
-    require 'twitter'
+get '/tweets' do
+    require "twitter"
     client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = "KiTNpI4rqUWeuh0KR9CB32lyV"
-        config.consumer_secret     = "emN6pNQlSqN7WfP7xaWBhNGSh2tGBv7Ugi9XN7JCWKg22CUNGX"
-        config.access_token        = "51299621-Rpoj1F586SXNLQrDYgEagdbDSib2tJIewdkr6rzS6"
-        config.access_token_secret = "2EOto7unip5Ebot0bCGPqzZFw2lLUMQLGcd92HZikGNcX"
-end
-   
-    @search_results = client.search("@mercerfootball", result_type: "recent").take(30).collect do |tweet|
-      #"#{tweet.user.screen_name}: #{tweet.text}"
-        tweet 
-end
+      config.consumer_key        = "l1jE08nkBTaDjMbWzESb0ESKE"
+      config.consumer_secret     = "isu6DNE5ec2brHFD4EH1lH2fEl1VXpgZy2JQD8wZ91I6miiDqI"
+      config.access_token        = "2882691561-mJ9F4X53UjWrPuwj0cjrN8T8ZGpdI2nc5jtaSNp"
+      config.access_token_secret = "0wTZww32d2zsTIzrLisdi88GIdtA3QdsrKOvlN4VVxUF5"
+    end
     
-    @title = "My _____ Tweets"
-    @description = "This page my tweets that ______"
-    @works = "active"
+   @search_results = client.search("@Mercerfootball", result_type: "recent").take(15).collect do |tweet|
+    #"#{tweet.user.screen_name}: #{tweet.text}"
+       tweet
+    end 
+    
+    @title = "Tweets"
+    @discripton = "This page provides links to a couple of recent tweets about Mercer football. I don't tweet a lot!!"
+    @tweets = "active"
     erb :tweets
-end 
+end
 
 get '/news' do 
     require 'google-search'
